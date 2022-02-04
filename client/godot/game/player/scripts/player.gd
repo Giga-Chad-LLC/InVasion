dab3e2303a1a89f8cc62137eccd1315cb19ebfb4
Dmitrii Art <artuhovdmitrii@gmail.com>
2022-02-04 20:30:55 +0300

    make the boilerplate for game, add basic game mechanics

---
+++ client/godot/game/player/scripts/player.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+
+export var ACCELERATION = 500
+export var MAX_SPEED = 100
+export var FRICTION = 500
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+
+func _physics_process(delta):
+	move_and_animate_player(delta) # move the player
+
+
+func move_and_animate_player(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
