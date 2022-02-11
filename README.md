51831b41486d46b31c7d372ed7f931341bc94b89
Nikolai <be2carefull@yandex.ru>
2022-02-11 21:20:05 +0300

    prototype of server and client

--- README.md
+++
@@ -1,3 +0,0 @@
-# InVasion
-Here will be the merge of `dev/client` and `dev/servers` branches.
-We have splitted out the codebases for client and servers due to their independence of each other (for cross OS development: WSL2 for servers + Windows10 for client).
---
+++ servers/server.cc
@@ -0,0 +1,14 @@
+#include "proto/server.pb.h"
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+//using helloworld::Greeter;
+//using helloworld::HelloReply;
+//using helloworld::HelloRequest;
+int main(){}
