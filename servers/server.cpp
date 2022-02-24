963693d4f3c98eeddb9c0c47e46b983384948f5b
Nikolai <be2carefull@yandex.ru>
2022-02-24 23:47:55 +0300

    server.cpp: start work undo server

--- servers/server.cpp
+++ servers/server.cpp
@@ -14,31 +14,38 @@ using boost::asio::ip::tcp;
 int main(int argc, char *argv[]) {
 
 
-	boost::asio::io_context io_context;
-	tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8000));
-	std::cout << "Listening at " << acceptor.local_endpoint() << std::endl;
+    boost::asio::io_context io_context;
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8000));
+    std::cout << "Listening at " << acceptor.local_endpoint() << std::endl;
 
-	while (true) {
-		tcp::socket s = acceptor.accept();
-		
-		std::thread([s = std::move(s)]() mutable {
-			std::cout << "Connected client: " << s.remote_endpoint() << " --> " << s.local_endpoint() << std::endl;
-			tcp::iostream client(std::move(s));
+    while (true) {
+        tcp::socket s = acceptor.accept();
 
-			client << "Hello from C++ server!\n";
+        std::thread([s = std::move(s)]() mutable {
+            std::cout << "Connected client: " << s.remote_endpoint() << " --> " << s.local_endpoint() << std::endl;
+//            boost::asio::streambuf buf;
+//            boost::asio::read_until( s, buf, '\0' );
+//            std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+//            std::cout << data << "124";
+            tcp::iostream client(std::move(s));
+            std::string mes;
+            client << "Hello from C++ server!\n";
+            while (client) {
+                std::string buffer;
+                PlayerAction tmp_;
+                if (client >> buffer){
+                    std::cout << buffer << '\n';
+                }
 
-			while (client) {
-				std::string msg;
+                tmp_.ParseFromString(buffer);
+                std::string answer_from_server;
+                std::cout << tmp_.key_pressed() << '\n';
+                tmp_.SerializeToString(&answer_from_server);
+                client << answer_from_server;
+            }
+//                client >> buffer;
 
-				if (!(client >> msg)) {
-					break;
-				}
-
-				client << "take it back: " << msg << std::endl;
-				std::cout << "Received: " << msg << std::endl;
-			}
-
-			std::cout << "Disconnected" << std::endl;
-		}).detach();
-	}
+            std::cout << "Disconnected" << std::endl;
+        }).detach();
+    }
 }
