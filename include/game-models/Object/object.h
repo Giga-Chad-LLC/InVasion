11c8d765240e2e9f461c19d5fb3f9f2b01c2c40f
TommyIQ <bmp110676@gmail.com>
2022-02-24 15:17:58 +0300

    Create basic structure for Object

---
+++ include/game-models/Object/object.h
@@ -0,0 +1,28 @@
+#ifndef OBJECT_H_
+#define OBJECT_H_
+
+#include "game-models/Vector2D/vector2d.h"
+#include "game-models/BoxCollider/box-collider.h"
+
+
+
+namespace invasion::game_models {
+
+struct Object {
+	explicit Object(Vector2D collider_size, Vector2D initial_pos);
+
+	Vector2D getPosition() const;
+
+	void setPosition(Vector2D pos);
+
+protected:
+	const BoxCollider m_collider;
+	Vector2D m_position;
+};
+
+
+} // namespace invasion::game_models
+
+
+
+#endif // OBJECT_H_
