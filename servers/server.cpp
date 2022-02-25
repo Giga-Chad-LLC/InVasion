60125cc7646a46d5e8ba9b9fc8ae32b2bc5281c2
Nikolai <be2carefull@yandex.ru>
2022-02-25 21:04:18 +0300

    simple server

--- servers/server.cpp
+++ servers/server.cpp
@@ -1,18 +1,25 @@
 #include <algorithm>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <thread>
 #include <utility>
-#include "./cmake-build-debug/player.pb.h"
+#include "player.pb.h"
 
 using boost::asio::ip::tcp;
 
 
+std::string make_string(boost::asio::streambuf &streambuf) {
+    return {
+            boost::asio::buffers_begin(streambuf.data()),
+            boost::asio::buffers_end(streambuf.data())
+    };
+}
+
 // NOLINTNEXTLINE
 int main(int argc, char *argv[]) {
-
 
     boost::asio::io_context io_context;
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8000));
@@ -21,31 +28,28 @@ int main(int argc, char *argv[]) {
     while (true) {
         tcp::socket s = acceptor.accept();
 
-        std::thread([s = std::move(s)]() mutable {
-            std::cout << "Connected client: " << s.remote_endpoint() << " --> " << s.local_endpoint() << std::endl;
-//            boost::asio::streambuf buf;
-//            boost::asio::read_until( s, buf, '\0' );
-//            std::string data = boost::asio::buffer_cast<const char*>(buf.data());
-//            std::cout << data << "124";
-            tcp::iostream client(std::move(s));
-            std::string mes;
-            client << "Hello from C++ server!\n";
+        std::thread([socket = std::move(s)]() mutable {
+            std::cout << "Connected client: " << socket.remote_endpoint() << " --> " << socket.local_endpoint()
+                      << std::endl;
+            tcp::iostream client(std::move(socket));
+            client << "Hello from C++ server!" << std::endl;
+
             while (client) {
-                std::string buffer;
-                PlayerAction tmp_;
-                if (client >> buffer){
-                    std::cout << buffer << '\n';
-                }
 
-                tmp_.ParseFromString(buffer);
-                std::string answer_from_server;
-                std::cout << tmp_.key_pressed() << '\n';
-                tmp_.SerializeToString(&answer_from_server);
-                client << answer_from_server;
+                std::uint32_t size;  // get the message data length in bytes
+                client.read(reinterpret_cast<char *> (&size), sizeof(size));
+                char arr[size];
+                client.read(reinterpret_cast<char *> (&arr), size);
+
+                PlayerAction action;
+                action.ParseFromArray(arr, size);
+                // std::cout << "Action retrieve: " << action.key_pressed() << ", size in bytes: " << action.ByteSizeLong() << std::endl;
+                char action_buffer[action.ByteSizeLong()];
+                action.SerializeToArray(action_buffer, action.ByteSizeLong());
+
+                client.write(action_buffer, action.ByteSizeLong());
             }
-//                client >> buffer;
-
-            std::cout << "Disconnected" << std::endl;
+            std::cout << "Client disconnected" << std::endl;
         }).detach();
     }
 }
