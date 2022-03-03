0940f4102be155b7f7c077cbe47994b318b782f8
Nikolai <be2carefull@yandex.ru>
2022-03-03 20:11:00 +0300

    implemented class Client, thread make_receiver

--- include/server-include/server.h
+++ include/server-include/server.h
@@ -12,51 +12,92 @@
 #include <string>
 #include <thread>
 #include <utility>
+
 using boost::asio::ip::tcp;
 
-void return_client(tcp::socket && socket){
-        std::cout << "Connected client: " << socket.remote_endpoint() << " --> " << socket.local_endpoint()
-                  << std::endl;
-        tcp::iostream client(std::move(socket));
-        client << "Hello from C++ server!" << std::endl;
+//void handler(SafeQueue<PlayerAction> *q){
+//    while(true){
+//        while(q->Size() > 0){
+//
+//        }
+//    }
+//}
+class Client{
+private:
+    tcp::iostream chanel;
+    SafeQueue<PlayerAction> queue_send;
+    friend void make_receiver(std::shared_ptr<Client> client);
+public:
+    Client(tcp::socket &&socket) : chanel(std::move(socket)){
+        chanel << "Hello from C++ server!" << std::endl;
+    }
+};
 
-        while (client) {
+
+void make_receiver(std::shared_ptr<Client> client){
+
+        while (client->chanel) {
 
             std::uint32_t size;  // get the message data length in bytes
-            client.read(reinterpret_cast<char *> (&size), sizeof(size));
+            client->chanel.read(reinterpret_cast<char *> (&size), sizeof(size));
             char arr[size];
-            client.read(reinterpret_cast<char *> (&arr), size);
+            client->chanel.read(reinterpret_cast<char *> (&arr), size);
 
             PlayerAction action;
             action.ParseFromArray(arr, size);
-            std::cout << "Action retrieve: " << action.key_pressed() << ", size in bytes: " << action.ByteSizeLong() << std::endl;
+//        q->Produce(std::move(action));
+
+            std::cout << "Action retrieve: " << action.key_pressed() << ", size in bytes: " << action.ByteSizeLong()
+                      << std::endl;
             char action_buffer[action.ByteSizeLong()];
             action.SerializeToArray(action_buffer, action.ByteSizeLong());
 
-            client.write(action_buffer, action.ByteSizeLong());
+            client->chanel.write(action_buffer, action.ByteSizeLong());
         }
         std::cout << "Client disconnected" << std::endl;
 
 }
 
+void make_sender(SafeQueue<PlayerAction> *q, tcp::iostream *client){
+
+}
+
+
+//void return_client(tcp::socket &&socket, SafeQueue<PlayerAction> *queue_receive) {
+//
+//    SafeQueue<PlayerAction> queue_send;
+//
+//    std::thread a (make_receiver, queue_receive, &client);
+//    a.detach();
+//    while(true){
+//
+//    }
+////    std::thread(make_sender, &queue_send, &client).detach();
+//
+//
+//
+//}
+
 class server {
 private:
-    SafeQueue<PlayerAction> queue_receive;
-    SafeQueue<PlayerAction> queue_send;
     boost::asio::io_context io_context;
     tcp::acceptor acceptor;
+    std::vector<std::shared_ptr<Client>> base_clients;
 public:
-    server() : acceptor(io_context, tcp::endpoint(tcp::v4(), 8000)){
+    SafeQueue<PlayerAction> queue_receive;
+    server() : acceptor(io_context, tcp::endpoint(tcp::v4(), 8000)) {
         std::cout << "Listening at " << acceptor.local_endpoint() << std::endl;
-        wait_new_client();
     }
 
     void wait_new_client() {
 
         while (true) {
             tcp::socket s = acceptor.accept();
-
-            std::thread(return_client, std::move(s)).detach();
+            std::cout << "Connected client: " << s.remote_endpoint() << " --> " << s.local_endpoint()
+                      << std::endl;
+            auto pointer_on_user = std::make_shared<Client>(std::move(s));
+            base_clients.push_back(pointer_on_user);
+            std::thread(make_receiver, pointer_on_user).detach();
         }
     }
 };
--- src/server_src/main.cpp
+++ src/server_src/main.cpp
@@ -2,4 +2,5 @@
 
 int main(){
     server cur_server;
+    cur_server.wait_new_client();
 }
