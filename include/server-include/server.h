1e5f844acd60741fa0e66bc077e2328f242828c7
Nikolai <be2carefull@yandex.ru>
2022-03-03 20:52:42 +0300

    it work!

--- include/server-include/server.h
+++ include/server-include/server.h
@@ -15,18 +15,14 @@
 
 using boost::asio::ip::tcp;
 
-//void handler(SafeQueue<PlayerAction> *q){
-//    while(true){
-//        while(q->Size() > 0){
-//
-//        }
-//    }
-//}
+
 class Client{
 private:
     tcp::iostream chanel;
     SafeQueue<PlayerAction> queue_send;
-    friend void make_receiver(std::shared_ptr<Client> client);
+    friend void make_receiver(std::shared_ptr<Client> client,SafeQueue<PlayerAction> *q);
+    friend void make_sender(std::shared_ptr<Client> client);
+    friend void handler(SafeQueue<PlayerAction> *q);
 public:
     Client(tcp::socket &&socket) : chanel(std::move(socket)){
         chanel << "Hello from C++ server!" << std::endl;
@@ -34,7 +30,7 @@ public:
 };
 
 
-void make_receiver(std::shared_ptr<Client> client){
+void make_receiver(std::shared_ptr<Client> client, SafeQueue<PlayerAction> *q){
 
         while (client->chanel) {
 
@@ -45,20 +41,31 @@ void make_receiver(std::shared_ptr<Client> client){
 
             PlayerAction action;
             action.ParseFromArray(arr, size);
-//        q->Produce(std::move(action));
+            q->Produce(std::move(action));
 
-            std::cout << "Action retrieve: " << action.key_pressed() << ", size in bytes: " << action.ByteSizeLong()
-                      << std::endl;
-            char action_buffer[action.ByteSizeLong()];
-            action.SerializeToArray(action_buffer, action.ByteSizeLong());
 
-            client->chanel.write(action_buffer, action.ByteSizeLong());
+//            std::cout << "Action retrieve: " << action.key_pressed() << ", size in bytes: " << action.ByteSizeLong()
+//                      << std::endl;
+//            char action_buffer[action.ByteSizeLong()];
+//            action.SerializeToArray(action_buffer, action.ByteSizeLong());
+//
+//            client->chanel.write(action_buffer, action.ByteSizeLong());
         }
         std::cout << "Client disconnected" << std::endl;
 
 }
 
-void make_sender(SafeQueue<PlayerAction> *q, tcp::iostream *client){
+void make_sender(std::shared_ptr<Client> client){
+    while(true){
+            PlayerAction action;
+            if (client->queue_send.Consume(action)){
+                std::cout << "OK";
+                char action_buffer[action.ByteSizeLong()];
+                action.SerializeToArray(action_buffer, action.ByteSizeLong());
+
+                client->chanel.write(action_buffer, action.ByteSizeLong());
+            }
+        }
 
 }
 
@@ -77,29 +84,56 @@ void make_sender(SafeQueue<PlayerAction> *q, tcp::iostream *client){
 //
 //
 //}
+std::vector<std::shared_ptr<Client>> base_clients;
 
+void handler(SafeQueue<PlayerAction> *q);
 class server {
 private:
     boost::asio::io_context io_context;
     tcp::acceptor acceptor;
-    std::vector<std::shared_ptr<Client>> base_clients;
+
+    bool handler_implemented = false;
 public:
     SafeQueue<PlayerAction> queue_receive;
     server() : acceptor(io_context, tcp::endpoint(tcp::v4(), 8000)) {
         std::cout << "Listening at " << acceptor.local_endpoint() << std::endl;
+
     }
-
+    void make_handler(){
+        if (!handler_implemented){
+            std::thread(handler, &queue_receive).detach();
+            handler_implemented = true;
+        }
+    }
     void wait_new_client() {
-
+        bool flag = true;
         while (true) {
             tcp::socket s = acceptor.accept();
             std::cout << "Connected client: " << s.remote_endpoint() << " --> " << s.local_endpoint()
                       << std::endl;
             auto pointer_on_user = std::make_shared<Client>(std::move(s));
             base_clients.push_back(pointer_on_user);
-            std::thread(make_receiver, pointer_on_user).detach();
+            std::thread(make_receiver, pointer_on_user, &queue_receive).detach();
+            std::thread(make_sender, pointer_on_user).detach();
+            if (base_clients.size() == 1 && flag){
+                std::cout << "team are full\n";
+                flag = false;
+                make_handler();
+            }
         }
     }
 };
 
+void handler(SafeQueue<PlayerAction> *q){ // если у нас подключатся юзеры после метода make_handler, то он их просто не увидит
+    while(true){
+        PlayerAction cur;
+        if (q->Consume(cur)){
+            for (auto cur_client : base_clients){
+                // пока никакой обработки
+                PlayerAction tmp = cur;
+                cur_client->queue_send.Produce(std::move(tmp));
+            }
+        }
+    }
+}
 #endif //INVASION_SERVER_SERVER_H
--- src/server_src/main.cpp
+++ src/server_src/main.cpp
@@ -3,4 +3,5 @@
 int main(){
     server cur_server;
     cur_server.wait_new_client();
+
 }
