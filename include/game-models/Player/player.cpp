cb717d2e843d0f8f8bb82172db3e904e7b4b745b
TommyIQ <bmp110676@gmail.com>
2022-03-11 19:59:54 +0300

    Add weapon member to Player

--- include/game-models/Player/player.cpp
+++ include/game-models/Player/player.cpp
@@ -5,6 +5,8 @@
 namespace invasion::game_models {
 const double Player::MAX_SPEED = Vector2D(10, 0).magnitude();
 const double Player::MASS = 60.0;
+const int Player::INITIAL_AMMO = 180;
+const double Player::DAMAGE = 15.0;
 const Vector2D Player::COLLIDER_SIZE(20, 20);
 
 	
@@ -14,10 +16,16 @@ Player::Player(Vector2D initial_pos, const int playerId)
 		std::move(initial_pos), 
 		Player::MASS,
 		Player::MAX_SPEED
-	), m_id(playerId) {}
+	), 
+	m_id(playerId),
+	m_weapon(playerId, Player::INITIAL_AMMO, Player::DAMAGE) {}
 
 int Player::getId() const {
 	return m_id;
+}
+
+Weapon& Player::getWeapon() {
+	return m_weapon;
 }
 
 } // namespace invasion::game_models
