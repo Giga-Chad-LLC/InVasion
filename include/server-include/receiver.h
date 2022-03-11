2b587582bdb320deddaa6852f08ec4c0fe820caa
Nikolai <be2carefull@yandex.ru>
2022-03-11 18:01:32 +0300

    fix style read and write response and request in receiver and sender

--- include/server-include/receiver.h
+++ include/server-include/receiver.h
@@ -12,22 +12,30 @@
 
 namespace invasion::session {
     class ReceiverFromUser {
+    private:
+        std::shared_ptr<User> clientPointer;
+
+        NetworkPacketRequest readClient() {
+            std::uint32_t size;  // get the message data length in bytes
+            clientPointer->channel.read(reinterpret_cast<char *> (&size), sizeof(size));
+            std::uint32_t messageType; // get the message type
+            clientPointer->channel.read(reinterpret_cast<char *> (&messageType), sizeof(messageType));
+
+            std::unique_ptr<char> buffer_ptr(new char[size]);
+            NetworkPacketRequest packet(std::move(buffer_ptr), NetworkPacketRequest::getMessageTypeById(messageType),
+                                        size);
+            packet.setPlayerId(clientPointer->getIdClient()); //set idPlayer
+            clientPointer->channel.read(reinterpret_cast<char *> (packet.getStoredBytes()), size);
+            return packet;
+        }
 
     public:
-        ReceiverFromUser(std::shared_ptr<User> cur_client, SafeQueue<NetworkPacketRequest> *queueServerFromClients) {
-            std::thread([client = std::move(cur_client), q = queueServerFromClients]() {
+        ReceiverFromUser(std::shared_ptr<User> cur_client, SafeQueue<NetworkPacketRequest> *queueServerFromClients)
+                : clientPointer(cur_client) {
+            std::thread([client = std::move(cur_client), q = queueServerFromClients, this]() {
                 while (client->channel) {
                     // get data from client
-                    std::uint32_t size;  // get the message data length in bytes
-                    client->channel.read(reinterpret_cast<char *> (&size), sizeof(size));
-                    std::uint32_t messageType; // get the message type
-                    client->channel.read(reinterpret_cast<char *> (&messageType), sizeof(messageType));
-                    
-                    std::unique_ptr <char> buffer_ptr(new char[size]);
-                    NetworkPacketRequest packet(std::move(buffer_ptr), NetworkPacketRequest::getMessageTypeById(messageType), size);
-                    packet.setPlayerId(client->getIdClient()); //set idPlayer
-                    client->channel.read(reinterpret_cast<char*> (packet.getStoredBytes()), size);
-
+                    auto packet = readClient();
                     q->produce(std::move(packet));
                 }
                 std::cout << "Client disconnected" << std::endl;
--- include/server-include/sender.h
+++ include/server-include/sender.h
@@ -13,9 +13,20 @@
 
 namespace invasion::session {
     class SenderUser {
+    private:
+        std::shared_ptr<char> returnArrayBytes(NetworkPacketResponse &response) {
+            std::uint32_t type = static_cast<std::uint32_t> (response.getMessageType());
+            char *action_buffer = new char[response.bytesSize() + sizeof(type)];
+            std::shared_ptr<char> action_buffer_ptr(action_buffer);
+            std::memcpy(action_buffer_ptr.get(), reinterpret_cast<char *> (&type), sizeof(type));
+            std::memcpy(action_buffer_ptr.get() + static_cast<int> (sizeof(type)), response.getStoredBytes(),
+                        response.bytesSize());
+            return action_buffer_ptr;
+        }
+
     public:
         SenderUser(std::shared_ptr<User> curClient) {
-            std::thread([client = curClient]() {
+            std::thread([client = curClient, this]() {
                 while (true) {
 
                     NetworkPacketResponse response;
@@ -23,13 +34,9 @@ namespace invasion::session {
                         switch (response.getMessageType()) {
                             case ResponseModel_t::PlayerActionResponseModel: {
                                 std::uint32_t type = static_cast<std::uint32_t> (response.getMessageType());
-                                char* action_buffer = new char[response.bytesSize() + sizeof(type)];
-                                std::unique_ptr <char> action_buffer_ptr(action_buffer); 
-
-                                std::memcpy(action_buffer_ptr.get(), reinterpret_cast<char*> (&type), sizeof(type));
-                                std::memcpy(action_buffer_ptr.get() + static_cast<int> (sizeof(type)), response.getStoredBytes(), response.bytesSize());
-
-                                client->channel.write(action_buffer_ptr.get(), response.bytesSize() + sizeof(type));
+                                auto numberBytes = response.bytesSize() + sizeof(type);
+                                auto responseFromServer = returnArrayBytes(response);
+                                client->channel.write(responseFromServer.get(), numberBytes);
                                 break;
                             }
                             default: {
