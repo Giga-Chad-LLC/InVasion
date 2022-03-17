e2291a703b09e31ca5962e743b5bda376f46b24c
TommyIQ <bmp110676@gmail.com>
2022-03-17 20:03:02 +0300

    Make stop() be called only once in PhysicsTickController

--- include/controllers/PhysicsTickController/physics-tick-controller.cpp
+++ include/controllers/PhysicsTickController/physics-tick-controller.cpp
@@ -1,18 +1,25 @@
 #include <atomic>
 #include "physics-tick-controller.h"
 
+#include <iostream>
+
 namespace invasion::controllers {
 
 PhysicsTickController::PhysicsTickController(const std::size_t interval)
-	: m_cancelToken(false), m_interval_ms(interval) {}
+	: m_cancelToken(false), 
+	 m_isThreadActive(false),
+	  m_interval_ms(interval) {}
 
 void PhysicsTickController::stop() {
-	m_cancelToken.store(false);
-	m_startedThread.join();
+	if(m_isThreadActive.load()) {
+		m_cancelToken.store(false);
+		m_startedThread.join();
+
+		m_isThreadActive.store(false);
+	}
 }
 
 PhysicsTickController::~PhysicsTickController() {
-	// calling stop() to finish created thread
 	stop();
 }
 
