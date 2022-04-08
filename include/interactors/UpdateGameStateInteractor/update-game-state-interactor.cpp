960b718a92165d780060bf413aa0a5ce9f739511
TommyIQ <bmp110676@gmail.com>
2022-04-08 21:28:34 +0300

    Make UpdateGameStateInteractor create players positions response model

--- include/interactors/UpdateGameStateInteractor/update-game-state-interactor.cpp
+++ include/interactors/UpdateGameStateInteractor/update-game-state-interactor.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
+#include <vector>
 
 #include "update-game-state-interactor.h"
+// game-models
 #include "game-models/GameSession/game-session.h"
 #include "game-models/Player/player.h"
-
+#include "game-models/Vector2D/vector2d.h"
+// response models
 #include "player-position-response-model.pb.h"
+#include "players-positions-response-model.pb.h"
+// utils
 #include "vector2d.pb.h"
+
 
 namespace invasion::interactors {
 using namespace invasion::game_models;
 
-response_models::PlayerPositionResponseModel UpdateGameStateInteractor::execute(GameSession& session) const {
-	
+
+response_models::PlayersPositionsResponseModel UpdateGameStateInteractor::execute(GameSession& session) const {
+	// updating game
 	session.updateGameState();
+
+	response_models::PlayersPositionsResponseModel response;
+	
+	const std::vector<Player>& players = session.getPlayers();
+	
+	for(const auto& player : players) {
+		response_models::PlayerPositionResponseModel* playerModel = response.add_players();
+
+		playerModel->set_playerid(player.getId());
+		
+		if(player.getTeamId() == Player::TeamId::FirstTeam) {
+			playerModel->set_team_id(response_models::PlayerPositionResponseModel::FirstTeam);
+		}
+		else {
+			playerModel->set_team_id(response_models::PlayerPositionResponseModel::SecondTeam);
+		}
+
+		const Vector2D velocity = player.getVelocity();
+		playerModel->mutable_velocity()->set_x(velocity.getX());
+		playerModel->mutable_velocity()->set_y(velocity.getY());
+
+		const Vector2D position = player.getPosition();
+		playerModel->mutable_position()->set_x(position.getX());
+		playerModel->mutable_position()->set_y(position.getY());
+	}
+
+	/*
 	
 	// hardcoding exactly one player
 	const int playerId = 0;
@@ -27,6 +61,8 @@ response_models::PlayerPositionResponseModel UpdateGameStateInteractor::execute(
 	// setting velocity
 	response.mutable_velocity()->set_x(player.getVelocity().getX());
 	response.mutable_velocity()->set_y(player.getVelocity().getY());
+
+	*/
 
 	return response;
 }
--- include/interactors/UpdateGameStateInteractor/update-game-state-interactor.h
+++ include/interactors/UpdateGameStateInteractor/update-game-state-interactor.h
@@ -1,14 +1,18 @@
 #ifndef UPDATE_GAME_STATE_INTERACTOR_H_
 #define UPDATE_GAME_STATE_INTERACTOR_H_
 
+// game-models
 #include "game-models/GameSession/game-session.h"
+// response models
 #include "player-position-response-model.pb.h"
+#include "players-positions-response-model.pb.h"
+
 
 namespace invasion::interactors {
 using namespace invasion::game_models;
 
 struct UpdateGameStateInteractor {
-	response_models::PlayerPositionResponseModel execute(GameSession& session) const;
+	response_models::PlayersPositionsResponseModel execute(GameSession& session) const;
 };
 
 
