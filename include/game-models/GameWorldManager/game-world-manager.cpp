3c5bf4b02927c15fb3ae7aaa6ce2567215ede9c7
TommyIQ <bmp110676@gmail.com>
2022-04-09 14:39:47 +0300

    Refactor code in updatePlayersPositions method of GameWorldManager

--- include/game-models/GameWorldManager/game-world-manager.cpp
+++ include/game-models/GameWorldManager/game-world-manager.cpp
@@ -15,45 +15,35 @@ void GameWorldManager::updatePlayersPositions(std::vector<Player>& players, doub
 
 
 	for(Player& player : players) {
+		Vector2D resultForce = Vector2D::ZERO;
+
 		// applying friction force
 		if(player.isMoving()) {
 			const Vector2D appliedForce = Vector2D::clampMagnitude(player.getMovingForce(), appliedForceMagnitude);
 			const Vector2D frictionForce = 500.0 * frictionCoef * appliedForce.normalize();
 
-			//std::cout << "appliedForce magnitude: " << appliedForce.magnitude() << std::endl;
-			//std::cout << "frictionForce magnitude: " << frictionForce.magnitude() << std::endl;
-
-			Vector2D resultForce = Vector2D::ZERO;
-		
 			if(frictionForce.magnitude() < appliedForce.magnitude()) {
 				resultForce = appliedForce + frictionForce;
 			}
-
-			player.setResultForce(resultForce);
-
-			//std::cout << "resultForce: " << resultForce << std::endl;			
 		}
 		else {
 			// applying friction until player stops
 			const Vector2D velocity = player.getVelocity();
 			const Vector2D frictionForce = 70.0 * frictionCoef * velocity.normalize();
-			const Vector2D acceleration = frictionForce / Player::MASS;
 
+			const Vector2D acceleration = frictionForce / Player::MASS;
 			const Vector2D integratedAcceleration = acceleration * dt;
-			// std::cout << "frictionForce magnitude: " << frictionForce.magnitude() << std::endl;
 
 			if(integratedAcceleration.magnitude() >= velocity.magnitude()) {
-				player.setResultForce(Vector2D::ZERO);
 				player.setVelocity(Vector2D::ZERO);
 			}
 			else {
-				player.setResultForce(frictionForce);
+				resultForce = frictionForce;
 			}
 		}
 
-		//std::cout << "velocity: " << player.getVelocity() << std::endl;
+		player.setResultForce(resultForce);
 		player.makeMove(dt);
-		//std::cout << "speed magnitude: " << player.getVelocity().magnitude() << std::endl;
 	}
 }
 
