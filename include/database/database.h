ab32f64ae13ab830f39dd2a4267c5db0e0f0f691
Nikolai <be2carefull@yandex.ru>
2022-04-15 00:14:16 +0300

    implemented class Database, added test for class Database, connected and configured bcrypter for hashing passwords and

--- include/database/database.h
+++ include/database/database.h
@@ -4,6 +4,14 @@
 #include <string>
 #include <sqlite3.h>
 #include "sqlite_orm.h"
+#include "../../src/bcrypt-for-password/include/bcrypt.h"
+#include "../../src/bcrypt-for-password/src/bcrypt_.h"
+
+#include "../../src/bcrypt-for-password/src/openbsd.h"
+#include "../../src/bcrypt-for-password/src/node_blf.h"
+
+#include "../../src/bcrypt-for-password/src/blowfish.h"
+#include "iostream"
 
 struct User {
     int id;
@@ -18,6 +26,7 @@ namespace {
 
 class Database {
 private:
+
     auto &getDatabase() {
         static auto storage_ = make_storage("db.sqlite",
                                             make_table("users",
@@ -26,6 +35,7 @@ private:
                                                        make_column("hashed password", &User::password)));
         return storage_;
     }
+
 public:
     bool tryAddUser(std::string nickname, std::string password) {
         static auto storage_ = getDatabase();
@@ -33,13 +43,13 @@ public:
         try {
             auto cntUsers = storage_.get_all<User>().size();
             if (cntUsers == 0) {
-                User user{-1, std::move(nickname), std::move(password)};
+                User user{-1, std::move(nickname), bcrypt::generateHash(password)};
                 storage_.insert(user);
                 return true;
             }
             auto listAllUsers = storage_.get_all<User>(where(c(&User::nickname) != nickname));
             if (listAllUsers.size() == cntUsers) {
-                User user{-1, nickname, password};
+                User user{-1, nickname, bcrypt::generateHash(password)};
                 storage_.insert(user);
                 return true;
             }
@@ -49,18 +59,34 @@ public:
             std::cout << e.what() << std::endl;
         }
     }
-//    bool LoginUser(const std::string nickname, const std::string password){
-//        static auto storage_ = getDatabase();
-//        storage_.sync_schema();
-//    }
-    std::vector<User> getArrayUsers(){
+
+    bool LoginUser(const std::string &nickname, const std::string &password) {
+        static auto storage_ = getDatabase();
+        storage_.sync_schema();
+        auto tmp = storage_.get_all_pointer<User>(where(c(&User::nickname) == nickname));
+        if (!tmp.size()) {
+            return false;
+        }
+        if (bcrypt::validatePassword(password, tmp[0]->password)) {
+            return true;
+        }
+        return false;
+    }
+
+    std::vector<User> getArrayUsers() {
         return getDatabase().get_all<User>();
     }
-    void printUsers(){
+
+    void printUsers() {
         auto arrayUsers = getArrayUsers();
-        for (auto user : arrayUsers){
-            std::cout << getDatabase().dump(user);
+        for (auto &user: arrayUsers) {
+            std::cout << getDatabase().dump(user) << std::endl;
         }
+    }
+
+    void deleteAllUsers() {
+        getDatabase().sync_schema();
+        getDatabase().remove_all<User>();
     }
 };
 
