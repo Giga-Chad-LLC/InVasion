ef06c422f3304d86920b9cc17ef53a2f4445caa8
Nikolai <be2carefull@yandex.ru>
2022-04-14 22:40:14 +0300

    implemented class Database

--- include/database/database.h
+++ include/database/database.h
@@ -12,37 +12,57 @@ struct User {
 };
 
 using namespace sqlite_orm;
-auto& getDatabase() {
-    static auto storage_ = make_storage("db.sqlite",
-                                make_table("users",
-                                           make_column("id", &User::id, autoincrement(), primary_key()),
-                                           make_column("nickname", &User::nickname),
-                                           make_column("hashed password", &User::password)));
-    return storage_;
+namespace {
+
 }
 
-
-bool tryAddUser(std::string nickname, std::string password){
-    try{
-        auto cntUsers = getDatabase().get_all<User>().size();
-        if (cntUsers == 0){
-            User user{-1, std::move(nickname), std::move(password)};
-            getDatabase().insert(user);
-            return true;
-        }
-        auto listAllUsers = getDatabase().get_all<User>(where(c(&User::nickname) != nickname));
-        if (listAllUsers.size() == cntUsers){
-            User user{-1, nickname, password};
-            getDatabase().insert(user);
-            return true;
-        }
-        return false;
+class Database {
+private:
+    auto &getDatabase() {
+        static auto storage_ = make_storage("db.sqlite",
+                                            make_table("users",
+                                                       make_column("id", &User::id, autoincrement(), primary_key()),
+                                                       make_column("nickname", &User::nickname),
+                                                       make_column("hashed password", &User::password)));
+        return storage_;
     }
-    catch(std::exception &e){
-        std::cout << e.what() << std::endl;
+public:
+    bool tryAddUser(std::string nickname, std::string password) {
+        static auto storage_ = getDatabase();
+        storage_.sync_schema();
+        try {
+            auto cntUsers = storage_.get_all<User>().size();
+            if (cntUsers == 0) {
+                User user{-1, std::move(nickname), std::move(password)};
+                storage_.insert(user);
+                return true;
+            }
+            auto listAllUsers = storage_.get_all<User>(where(c(&User::nickname) != nickname));
+            if (listAllUsers.size() == cntUsers) {
+                User user{-1, nickname, password};
+                storage_.insert(user);
+                return true;
+            }
+            return false;
+        }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
     }
-}
-
+//    bool LoginUser(const std::string nickname, const std::string password){
+//        static auto storage_ = getDatabase();
+//        storage_.sync_schema();
+//    }
+    std::vector<User> getArrayUsers(){
+        return getDatabase().get_all<User>();
+    }
+    void printUsers(){
+        auto arrayUsers = getArrayUsers();
+        for (auto user : arrayUsers){
+            std::cout << getDatabase().dump(user);
+        }
+    }
+};
 
 
 #endif //INVASION_DATABASE_H
