202efa65d0d0509f5ec80677f0c8caf514d4e3cd
unknown <artuhovdmitrii@gmail.com>
2022-04-21 20:16:29 +0300

    Add bullets deleting when out of bounds or hitting other player

--- client/godot/game/screens/game/scripts/world.gd
+++ client/godot/game/screens/game/scripts/world.gd
@@ -1,15 +1,10 @@
 extends Node2D
 
-export(int) var left_bound = -1000
-export(int) var right_bound = 1000
-export(int) var up_bound = -1000
-export(int) var down_bound = 1000
 
+# scenes
 var player_scene = preload("res://player/player_template.tscn")
 var bullet_scene = preload("res://models/bullet/bullet.tscn")
 # Godobuf
-const PlayersPositionsResponseModel = preload("res://proto/response-models/players_positions_response_model.gd")
-const BulletsPositionsResponseModel = preload("res://proto/response-models/bullets_positions_response_model.gd")
 const GameStateResponseModel = preload("res://proto/response-models/game_state_response_model.gd")
 
 func spawn_new_player(player_id, location):
@@ -44,19 +39,33 @@ func update_players_states(player_positions: Array):
 			spawn_new_player(player.get_player_id(),
 				Vector2(player.get_position().get_x(), player.get_position().get_y()))
 
+
+var all_bullets_on_map: Array = [] # stored strings (names of bullets in godot nodes hierarchy)
 func update_bullets_states(bullets_positions: Array):
 	var parent_node = get_tree().get_root().get_node("World/YSort/Bullets")
+	var existing_bullets: Dictionary = {}
+	
 	for i in range(0, bullets_positions.size()):
 		var bullet = bullets_positions[i]
 		var bullet_node_name = str(bullet.get_bullet_id())
+		existing_bullets[bullet_node_name] = true
+		
 		if (parent_node.has_node(bullet_node_name)):
 			# animate and move the bullet
 			parent_node.get_node(bullet_node_name).update_bullet_position(bullet)
 		else:
+			all_bullets_on_map.push_back(bullet_node_name)
 			spawn_new_bullet(bullet.get_bullet_id(), bullet.get_player_id(),
 				Vector2(bullet.get_position().get_x(), bullet.get_position().get_y()),
 				Vector2(bullet.get_velocity().get_x(), bullet.get_velocity().get_y()))
-
+	
+	var bullets_to_delete = []
+	for i in range(0, all_bullets_on_map.size()):
+		if (!existing_bullets.get(all_bullets_on_map[i], false)):
+			bullets_to_delete.push_back(i)
+	
+	for i in range(0, bullets_to_delete.size()):
+		despawn_bullet(all_bullets_on_map.pop_at(i))
 
 
 
