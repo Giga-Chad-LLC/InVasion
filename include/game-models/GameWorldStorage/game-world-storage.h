2cd52f5344c605fcccbc1a275209670faced721d
TommyIQ <bmp110676@gmail.com>
2022-04-21 18:48:10 +0300

    Rewrite code to use shared_ptr<Player> in GameWorldStorage

--- include/game-models/GameWorldStorage/game-world-storage.h
+++ include/game-models/GameWorldStorage/game-world-storage.h
@@ -12,11 +12,11 @@ namespace invasion::game_models {
 
 struct GameWorldStorage {
 	
-	std::vector<Player>& getPlayers();
+	std::vector<std::shared_ptr<Player>>& getPlayers();
 	std::vector<std::shared_ptr<Bullet>>& getBullets();
 
 private:
-	std::vector<Player> m_players;
+	std::vector<std::shared_ptr<Player>> m_players;
 	std::vector<std::shared_ptr<Bullet>> m_bullets;
 };
 
