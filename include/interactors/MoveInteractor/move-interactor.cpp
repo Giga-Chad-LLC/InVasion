85bb83e51b349021c4946aa67ec880c5c78c903f
TommyIQ <bmp110676@gmail.com>
2022-04-21 18:48:40 +0300

    Rewrite code to use shared_ptr<Player> in MoveInteractor

--- include/interactors/MoveInteractor/move-interactor.cpp
+++ include/interactors/MoveInteractor/move-interactor.cpp
@@ -1,39 +1,43 @@
+#include <memory>
+
 #include "move-interactor.h"
+// game-models
 #include "game-models/Vector2D/vector2d.h"
 #include "game-models/Player/player.h"
 #include "game-models/GameSession/game-session.h"
+
 
 
 namespace invasion::interactors {
 using namespace invasion::game_models;
 using namespace request_models;
 
-void MoveInteractor::execute(const request_models::MoveRequestModel& req, GameSession& session) const {
-	const request_models::MoveRequestModel::MoveEvent event = req.current_event();
-	Player& player = session.getPlayer(req.player_id());
+void MoveInteractor::execute(const MoveRequestModel& req, GameSession& session) const {
+	const MoveRequestModel::MoveEvent event = req.current_event();
+	std::shared_ptr<Player> player_ptr = session.getPlayer(req.player_id());
 
-	Vector2D moving_force = player.getMovingForce();
+	Vector2D moving_force = player_ptr->getMovingForce();
 
 	switch (event) {
 		// starti moving
 		case request_models::MoveRequestModel::StartMovingUp: {
 			moving_force.setY(-1);
-			player.setMovingState(true);
+			player_ptr->setMovingState(true);
 			break;
 		}
 		case request_models::MoveRequestModel::StartMovingRight: {
 			moving_force.setX(1);
-			player.setMovingState(true);
+			player_ptr->setMovingState(true);
 			break;
 		}
 		case request_models::MoveRequestModel::StartMovingDown: {
 			moving_force.setY(1);
-			player.setMovingState(true);
+			player_ptr->setMovingState(true);
 			break;
 		}
 		case request_models::MoveRequestModel::StartMovingLeft: {
 			moving_force.setX(-1);
-			player.setMovingState(true);
+			player_ptr->setMovingState(true);
 			break;
 		}
 		// stop moving
@@ -54,10 +58,10 @@ void MoveInteractor::execute(const request_models::MoveRequestModel& req, GameSe
 	
 	// if both components are zeros
 	if(moving_force.magnitude() < Vector2D::EPS) {
-		player.setMovingState(false);
+		player_ptr->setMovingState(false);
 	}
 
-	player.setMovingForce(moving_force);
+	player_ptr->setMovingForce(moving_force);
 }
 
 
