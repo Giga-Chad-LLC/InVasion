db44bf6f3b6e63e5e197ce05245d3124a8965de9
TommyIQ <bmp110676@gmail.com>
2022-04-21 17:51:22 +0300

    Update PlayersPositionsResponseInteractor
    Same reason as for BulletsPositionsResponseInteractor

--- include/interactors/PlayersPositionsResponseInteractor/players-positions-response-interactor.cpp
+++ include/interactors/PlayersPositionsResponseInteractor/players-positions-response-interactor.cpp
@@ -8,30 +8,29 @@
 #include "game-models/Vector2D/vector2d.h"
 // response models
 #include "player-position-response-model.pb.h"
-#include "players-positions-response-model.pb.h"
+#include "game-state-response-model.pb.h"
 // utils
 #include "vector2d.pb.h"
 
 
 namespace invasion::interactors {
 using namespace invasion::game_models;
+using namespace response_models;
 
-
-response_models::PlayersPositionsResponseModel PlayersPositionsResponseInteractor::execute(GameSession& session) const {
-	response_models::PlayersPositionsResponseModel response;
+void PlayersPositionsResponseInteractor::execute(GameStateResponseModel& response, GameSession& session) const {
 	
 	const std::vector<Player>& players = session.getPlayers();
 	
 	for(const auto& player : players) {
-		response_models::PlayerPositionResponseModel* playerModel = response.add_players();
+		PlayerPositionResponseModel* playerModel = response.add_players();
 
 		playerModel->set_player_id(player.getId());
 		
 		if(player.getTeamId() == Player::TeamId::FirstTeam) {
-			playerModel->set_team_id(response_models::PlayerPositionResponseModel::FirstTeam);
+			playerModel->set_team_id(PlayerPositionResponseModel::FirstTeam);
 		}
 		else {
-			playerModel->set_team_id(response_models::PlayerPositionResponseModel::SecondTeam);
+			playerModel->set_team_id(PlayerPositionResponseModel::SecondTeam);
 		}
 
 		const Vector2D velocity = player.getVelocity();
@@ -42,8 +41,6 @@ response_models::PlayersPositionsResponseModel PlayersPositionsResponseInteracto
 		playerModel->mutable_position()->set_x(position.getX());
 		playerModel->mutable_position()->set_y(position.getY());
 	}
-
-	return response;
 }
 
 } // namespace invasion::interactors
