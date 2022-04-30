521424fbdc40537b1e594d0d1a627a1b7cc6089a
Nikolai <be2carefull@yandex.ru>
2022-04-30 16:07:08 +0300

    Fix edits in database

---
+++ 3rd-party/common-header.h
@@ -0,0 +1,15 @@
+#ifndef INVASION_DATABASE_HEADER
+#define INVASION_DATABASE_HEADER
+
+#include <string>
+#include "sqlite_orm.h"
+#include "../src/bcrypt-for-password/include/bcrypt.h"
+#include "../src/bcrypt-for-password/src/bcrypt_.h"
+
+#include "../src/bcrypt-for-password/src/openbsd.h"
+#include "../src/bcrypt-for-password/src/node_blf.h"
+
+#include "../src/bcrypt-for-password/src/blowfish.h"
+#include "iostream"
+
+#endif //INVASION_DATABASE_HEADER
--- include/database/database.h
+++ include/database/database.h
@@ -3,7 +3,7 @@
 
 #include <sqlite3.h>
 //#include <optional>
-#include "common-header.h"
+#include "../../3rd-party/common-header.h"
 #include <functional>
 #include <optional>
 
@@ -15,22 +15,25 @@ struct User {
 
 using namespace sqlite_orm;
 
-namespace {
-    auto &getTable() {
-        auto table = make_table("users",
-                                make_column("id", &User::id, primary_key(), autoincrement()),
-                                make_column("nickname", &User::nickname, unique()),
-        make_column("hashed password", &User::password));
-        static auto storage_ = make_storage("db.sqlite", table);
+namespace invasion::database_access{
+    class DatabaseManager {
+    public:
+        static auto &getTable() {
+            auto table = make_table("users",
+                                    make_column("id", &User::id, primary_key(), autoincrement()),
+                                    make_column("nickname", &User::nickname, unique()),
+                                    make_column("hashed password", &User::password));
+            static auto storage_ = make_storage("db.sqlite", table);
+            storage_.sync_schema();
+            return storage_;
+        }
+    };
 
-        return storage_;
-    }
+    class DatabaseAccessor {
 
-
-    struct methodsDB {
+    public:
         static std::optional<std::string> getUserPassword(const std::string &nickname) {
-            auto table = getTable();
-            table.sync_schema();
+            auto table = DatabaseManager::getTable();
             auto selectedUsersArray = table.select(&User::password, where(c(&User::nickname) == nickname), limit(1));
             if (selectedUsersArray.empty()) {
                 return {};
@@ -39,14 +42,12 @@ namespace {
         }
 
         static bool checkUser(const std::string &nickname) {
-            auto table = getTable();
-            table.sync_schema();
+            auto table = DatabaseManager::getTable();
             return getUserPassword(nickname).has_value();
         }
 
         static std::optional<User> getUser(const std::string &nickname) {
-            auto table = getTable();
-            table.sync_schema();
+            auto table = DatabaseManager::getTable();
             auto selectedUsersArray = table.get_all<User>(where(c(&User::nickname) == nickname), limit(1));
             if (!selectedUsersArray.empty()) {
                 return {};
@@ -54,27 +55,17 @@ namespace {
             return selectedUsersArray[0];
         }
 
-        static std::optional<int> getUserId(const std::string &nickname) {
-
-        }
 
         static void insertUser(const User &user) {
-            auto table = getTable();
-            table.sync_schema();
-            try {
-                table.insert(user);
-            }
-            catch (std::exception &e) {
-                std::cout << e.what() << '\n';
-            }
+            auto table = DatabaseManager::getTable();
+            table.insert(user);
         }
 
         static bool deleteUserByNickname(const std::string &nickname) {
-            auto table = getTable();
-            table.sync_schema();
-            auto userID = getUserId(nickname);
+            auto table = DatabaseManager::getTable();
+            auto userID = getUser(nickname);
             if (userID.has_value()) {
-                table.remove<User>(userID.value());
+                table.remove<User>(userID.value().id);
                 return true;
             }
             return false;
@@ -83,38 +74,5 @@ namespace {
 }
 
 
-namespace Invasion::database {
-    struct InterfaceDB {
-        static bool login(const std::string &nickname, const std::string &password) {
-            auto hashedPassword = methodsDB::getUserPassword(nickname);
-            if (hashedPassword.has_value()) {
-                return bcrypt::validatePassword(password, hashedPassword.value());
-            }
-            return false;
-        }
 
-        static bool tryToRegistationUser(const std::string &nickname, const std::string &password) {
-            if (methodsDB::checkUser(nickname)) {
-                return false;
-            }
-            std::cout << "success registration\n";
-            methodsDB::insertUser(User{-1, nickname, bcrypt::generateHash(password)});
-            return true;
-        }
-
-        // these methods for debugging only
-        static void printUsers() {
-            auto arrayUsers = getTable().get_all<User>();
-            for (auto &user: arrayUsers) {
-                std::cout << getTable().dump(user) << std::endl;
-            }
-        }
-
-        static void deleteAllUsers() {
-            getTable().sync_schema();
-            getTable().remove_all<User>();
-        }
-    };
-
-}
 #endif //INVASION_DATABASE_H
