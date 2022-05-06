a04c09049848ba8ac7b57b99689542ac829c9222
nikolai <be2carefull@yandex.ru>
2022-05-06 16:18:52 +0200

    Add registration_screen, update README.md, update database

--- src/http-server/http_server.cpp
+++ src/http-server/http_server.cpp
@@ -1,10 +1,11 @@
 #include "crow_all.h"
-#include "../../include/database/database.h"
+#include "../../include/database/auth-service.h"
 #include <iostream>
 
 int main() {
-    using namespace Invasion::database;
-    InterfaceDB::deleteAllUsers();
+    using namespace invasion::database_access;
+    using namespace invasion::database_interface;
+    AuthService::deleteAllUsers();
     crow::SimpleApp app;
     CROW_ROUTE(app, "/registration")
             .methods("POST"_method)
@@ -12,14 +13,11 @@ int main() {
                         auto requestJson = crow::json::load(rowRequest.body);
                         crow::json::wvalue responseJson;
                         if (!requestJson) {
-                            responseJson["status code"] = 404;
                             responseJson["message"] = "Bad request";
-                        } else if (InterfaceDB::tryToRegistationUser(requestJson["nickname"].s(),
+                        } else if (AuthService::tryToRegisterUser(requestJson["nickname"].s(),
                                                                      requestJson["password"].s())) {
-                            responseJson["status code"] = 200;
                             responseJson["message"] = "Success registration!";
                         } else {
-                            responseJson["status code"] = 400;
                             responseJson["message"] = "This user already exists in the database!";
                             return crow::response(400, responseJson);
                         }
@@ -31,13 +29,10 @@ int main() {
                         auto requestJson = crow::json::load(rowRequest.body);
                         crow::json::wvalue responseJson;
                         if (!requestJson) {
-                            responseJson["status code"] = 404;
                             responseJson["message"] = "Bad request";
-                        } else if (InterfaceDB::login(requestJson["nickname"].s(), requestJson["password"].s())) {
-                            responseJson["status code"] = 200;
+                        } else if (AuthService::login(requestJson["nickname"].s(), requestJson["password"].s())) {
                             responseJson["message"] = "Success entry!";
                         } else {
-                            responseJson["status code"] = 400;
                             responseJson["message"] = "Wrong nickname or password!";
                         }
                         return responseJson;
