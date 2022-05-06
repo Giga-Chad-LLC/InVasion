2f6de3c408596dba792e778ebd04e40b4928df93
nikolai <be2carefull@yandex.ru>
2022-05-06 16:25:10 +0200

    Do registration screen

---
+++ client/godot/game/screens/registration_screen/RegistrationScreen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+signal scene_changed(scene_name)
+onready var start_game_button = $CenterContainer/VBoxContainer/Button
+onready var info_label = $"./LabelOfError"
+
+func _ready():
+	info_label.set("custom_colors/font_color", Color(1,0,0))
+	info_label.visible = false
+	
+
+func _on_GotoBack_pressed():
+	emit_signal("scene_changed", "start_menu")
+
+
+func _on_CheckButton_pressed():
+	var password = $"./CheckButton/PasswordEdit".get_text()
+	var username = $"./CheckButton/UsernameEdit".get_text()
+	# тут инициализация запроса к БД
+	if (password == "0000" and username == "aboba"):
+		# тут логика при успешном подключении
+		emit_signal("scene_changed", "start_menu")
+	else:
+		info_label.visible = true
