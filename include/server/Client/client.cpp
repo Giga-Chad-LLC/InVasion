0a9b14b43f3df384dfdba4c3a4e617cba8c75344
unknown <artuhovdmitrii@gmail.com>
2022-05-09 18:04:40 +0300

    Update client class

--- include/server/Client/client.cpp
+++ include/server/Client/client.cpp
@@ -1,13 +1,38 @@
 #include <memory>
 #include "client.h"
+#include "server/Session/session.h"
 
 
 namespace invasion::server {
 using boost::asio::ip::tcp;
 
-Client::Client(std::shared_ptr <tcp::socket> socket, uint32_t clientId): m_socket(socket), m_clientId(clientId) {}
+Client::Client(std::shared_ptr <tcp::socket> socket, uint32_t clientId): m_socket(socket), m_clientId(clientId), m_isActive(true) {
+    std::cout << "Crated new client" << std::endl;
+}
 
 void Client::stop() {
-    std::cout << "Stop client: " << m_socket->remote_endpoint() << std::endl;
+    std::cout << "Stop client (" << m_socket->remote_endpoint() << "): " << m_clientId << std::endl;
+}
+
+uint32_t Client::getClientId() const noexcept {
+    return m_clientId;
+}
+
+std::shared_ptr <tcp::socket> Client::getSocket() const noexcept {
+    return m_socket;
+}
+
+void Client::startReceivingPackets(
+    std::shared_ptr <SafeQueue<std::shared_ptr <NetworkPacketRequest>>> requestQueue,
+    std::shared_ptr <Session> session
+) {
+    
+}
+
+void Client::startSendingPackets(
+    std::shared_ptr <SafeQueue<std::shared_ptr <NetworkPacketResponse>>> clientResponseQueue,
+    std::shared_ptr <Session> session
+) {
+    
 }
 }
--- include/server/Client/client.h
+++ include/server/Client/client.h
@@ -5,6 +5,7 @@
 #include <boost/asio.hpp>
 #include <memory>
 #include <iostream>
+#include <atomic>
 // server
 #include "server/NetworkPacket/network-packet.h"
 #include "server/safe-queue.h"
@@ -20,18 +21,18 @@ public:
     void stop();
 
     void startReceivingPackets(
-        std::shared_ptr <SafeQueue<std::shared_ptr <NetworkPacket>>> requestQueue,
+        std::shared_ptr <SafeQueue<std::shared_ptr <NetworkPacketRequest>>> requestQueue,
         std::shared_ptr <Session> session
     );
 
     void startSendingPackets(
-        std::shared_ptr <SafeQueue<std::shared_ptr <NetworkPacket>>> clientResponseQueue,
+        std::shared_ptr <SafeQueue<std::shared_ptr <NetworkPacketResponse>>> clientResponseQueue,
         std::shared_ptr <Session> session
     );
 
-    uint32_t getClientId();
+    uint32_t getClientId() const noexcept;
 
-    std::shared_ptr <tcp::socket> getSocket();
+    std::shared_ptr <tcp::socket> getSocket() const noexcept;
 private:
     template <class Functor>
     void read(Functor callback) {}
@@ -42,6 +43,7 @@ private:
     
     const std::size_t MAX_MESSAGE_LENGTH = 1024U;
     uint32_t m_clientId;
+    std::atomic_bool m_isActive;
     std::shared_ptr <tcp::socket> m_socket;
     std::shared_ptr <char[]> m_readBuffer;
     std::shared_ptr <char[]> m_writeBuffer;
