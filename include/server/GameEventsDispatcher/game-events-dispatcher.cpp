2be1d78f1f53fc55b8061d590d1bdc0a59770353
unknown <artuhovdmitrii@gmail.com>
2022-05-09 18:04:59 +0300

    Update dispatcher class

--- include/server/GameEventsDispatcher/game-events-dispatcher.cpp
+++ include/server/GameEventsDispatcher/game-events-dispatcher.cpp
@@ -4,9 +4,18 @@
 
 namespace invasion::server {
 void GameEventsDispatcher::start() {
+    if (m_isActive.load()) {
+        return;
+    }
+
+    m_isActive.store(true);
     std::cout << "Game events dispatcher started" << std::endl;
 }
 void GameEventsDispatcher::stop() {
+    if (!m_isActive.load()) {
+        return;
+    }
+    m_isActive.store(false);
     std::cout << "Game events dispatcher stopped" << std::endl;
 }
 }
--- include/server/GameEventsDispatcher/game-events-dispatcher.h
+++ include/server/GameEventsDispatcher/game-events-dispatcher.h
@@ -1,12 +1,15 @@
 #ifndef GAME_EVENTS_DISPATCHER_H_
 #define GAME_EVENTS_DISPATCHER_H_
 
+#include "atomic"
+
 namespace invasion::server {
 class GameEventsDispatcher {
 public:
     void start();
     void stop();
 private:
+    std::atomic_bool m_isActive = false;
 };
 }
 
