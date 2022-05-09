b4d988733c02ca97fef8742eec6f6ba9e79fbdaf
unknown <artuhovdmitrii@gmail.com>
2022-05-09 15:02:03 +0300

    Prepare main for testing the server start and stop functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
-#include "server/Server/server.h"
 #include <exception>
+#include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 #include <iostream>
+#include <thread>
 #include <system_error>
+
+#include "server/Server/server.h"
+#include "controllers/FixedTimeoutCallbackInvoker/fixed-timeout-callback-invoker.h"
 
 
 int main() {
     try {
         invasion::server::Server server;
-        server.start("127.0.0.1", 8000);
-        system("pause");
-        // std::this_thread::sleep_for(std::chrono::seconds(60));
+        std::thread serverThread([&server]() {
+            server.start("127.0.0.1", 8000); // blocks the current thread of execution!
+        });
+        serverThread.detach();
+
+        invasion::controllers::FixedTimeoutCallbackInvoker timeout;
+        timeout.setTimeout(1000 * 10, [&serverThread, &server]() {
+            server.stop();
+            // serverThread.join();
+        });
+
+        // std::this_thread::sleep_for(std::chrono::seconds(6));
     }
     catch (boost::system::system_error& error) {
         std::cout << "Error while starting server: Error code = " << error.code() << ", Message: " << error.what() << std::endl;
     }
-
 }
