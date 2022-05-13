ee16f01d179527f0186c82f22b31c6113aadffe8
nikolai <be2carefull@yandex.ru>
2022-05-13 20:45:00 +0300

    Delete some headers, connect the server to the main game

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "server/server.h"
-
+#include "../include/http-server/http-server.h"
 int main() {
     invasion::session::Server server;
+    aboba();
     server.awaitNewConnections();
 }
