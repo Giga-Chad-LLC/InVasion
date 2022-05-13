39e14270e9bc399a035857b01a11b77cf32ab37a
nikolai <be2carefull@yandex.ru>
2022-05-13 20:35:48 +0300

    Create LobbyScreen.tscn

--- client/godot/game/screens/lobby_screen/LobbyScreen.gd
+++ client/godot/game/screens/lobby_screen/LobbyScreen.gd
@@ -4,9 +4,6 @@ signal scene_changed(scene_name)
 
 
 func _on_connectServerButton_pressed():
-	var ServerIP = $"./ServerIP".get_text()
-	print(ServerIP)
-	#обработка подключения
 	emit_signal("scene_changed", "game")
 
 
