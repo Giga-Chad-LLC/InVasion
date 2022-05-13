da2be3a146b9188b753ddc02f99acd1041322377
unknown <artuhovdmitrii@gmail.com>
2022-05-13 17:55:04 +0300

    Change players manager to match the removing functionality of bullets manager

--- client/godot/game/player/scripts/players_state_manager.gd
+++ client/godot/game/player/scripts/players_state_manager.gd
@@ -21,30 +21,42 @@ func despawn_player(player_id, players_parent_node):
 	if (players_parent_node and players_parent_node.has_node(str(player_id))):
 		players_parent_node.get_node(str(player_id)).queue_free()
 	else:
-		print("Error: Cannot despawn, players parent node is ", players_parent_node)
+		print("Error: Cannot despawn player with id " + str(player_id) + ", players parent node is ", players_parent_node)
 
 
-
+var players_on_map: Array = [] # stored ints (ids of players in godot nodes hierarchy)
 func update_players_states(player_positions: Array, main_player, players_parent_node):
 	if (!players_parent_node):
 		print("Error: Players parent node is ", players_parent_node)
 		return
 	
+	var existing_players: Dictionary = {}
+	
 	for i in range(0, player_positions.size()):
 		var model = player_positions[i]
+		existing_players[model.get_player_id()] = true
 		
 		if (model.get_player_id() == main_player.player_id):
 			main_player.update_player_position(model)
 		elif (players_parent_node.has_node(str(model.get_player_id()))): 
 			players_parent_node.get_node(str(model.get_player_id())).update_player_position(model)
 		else:
+			players_on_map.push_back(model.get_player_id())
 			var data = {
 				'player_id': model.get_player_id(),
 				'player_team_id': model.get_team_id(),
 				'local_team_id': main_player.team_id
 			}
 			spawn_player(data, players_parent_node, Vector2(model.get_position().get_x(), model.get_position().get_y()))
-
+	
+	# find players to delete from tree
+	var valid_players = []
+	for i in range(0, players_on_map.size()):
+		if (existing_players.get(players_on_map[i], false)):
+			valid_players.push_back(players_on_map[i])
+		else:
+			despawn_player(players_on_map[i], players_parent_node)
+	players_on_map = valid_players
 
 func update_damaged_players_states(damaged_players: Array, main_player, players_parent_node):
 	for i in range(damaged_players.size()):
