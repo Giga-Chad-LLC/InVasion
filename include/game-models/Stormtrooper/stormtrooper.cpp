78c0a5b4de628f10be738903ad306c87b5904c0d
TommyIQ <bmp110676@gmail.com>
2022-05-19 23:47:27 +0300

    Create Stormtrooper polymorphic subclass of Player

---
+++ include/game-models/Stormtrooper/stormtrooper.cpp
@@ -0,0 +1,26 @@
+#include "stormtrooper.h"
+
+// game-models
+#include "game-models/Player/player.h"
+#include "game-models/Vector2D/vector2d.h"
+
+
+namespace invasion::game_models {
+	
+
+Stormtrooper::Stormtrooper(Vector2D position, int playerId, PlayerTeamId teamId)
+	: Player(
+		Player::SHAPE_COLLIDER_SIZE,
+		Player::HITBOX_COLLIDER_SIZE,
+		position,
+		Player::MASS,
+		Stormtrooper::MAX_SPEED,
+		playerId,
+		teamId,
+		Stormtrooper::INITIAL_HIT_POINTS,
+		Stormtrooper::INITIAL_AMMO,
+		Stormtrooper::DAMAGE
+	) {};
+
+
+} // namespace invasion::game_models
---
+++ include/game-models/Stormtrooper/stormtrooper.h
@@ -0,0 +1,26 @@
+#ifndef STORMTROOPER_H_
+#define STORMTROOPER_H_
+
+// game-models
+#include "game-models/Player/player.h"
+#include "game-models/Vector2D/vector2d.h"
+
+namespace invasion::game_models {
+	
+
+class Stormtrooper : public Player {
+public:
+	explicit Stormtrooper(Vector2D position, int playerId, PlayerTeamId teamId);
+
+	inline static const double MAX_SPEED = 110.0;
+	inline static const int INITIAL_HIT_POINTS = 120;
+	inline static const double INITIAL_AMMO = 120;
+	inline static const double DAMAGE = 20;
+};
+
+
+} // namespace invasion::game_models
+
+
+
+#endif // STORMTROOPER_H_
