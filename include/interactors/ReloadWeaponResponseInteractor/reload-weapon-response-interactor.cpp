7e8704af38a6f787a267ef37d9ecde22c23aeb11
TommyIQ <bmp110676@gmail.com>
2022-05-27 19:05:16 +0300

    Create boilerplate for ReloadWeaponResponseInteractor

---
+++ include/interactors/ReloadWeaponResponseInteractor/reload-weapon-response-interactor.cpp
@@ -0,0 +1,8 @@
+#include "reload-weapon-response-interactor.h"
+
+
+/*
+
+TODO: write what should be done during execute() method
+
+*/
---
+++ include/interactors/ReloadWeaponResponseInteractor/reload-weapon-response-interactor.h
@@ -0,0 +1,19 @@
+#ifndef RELOAD_WEAPON_RESPONSE_INTERACTOR_H_
+#define RELOAD_WEAPON_RESPONSE_INTERACTOR_H_
+
+// game-models
+#include "game-models/GameSession/game-session.h"
+// response-models
+
+
+namespace invasion::interactors {
+	
+class ReloadWeaponResponseInteractor {
+	void execute() const;
+};
+
+} // namespace invasion::interactors
+
+
+
+#endif // RELOAD_WEAPON_RESPONSE_INTERACTOR_H_
