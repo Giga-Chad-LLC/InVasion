c3466730a77d498292fc6674ff4ebc70b3636a20
TommyIQ <bmp110676@gmail.com>
2022-05-28 17:59:38 +0300

    Rename bcrypt_.h to bcrypt-essential.h

--- include/libs/bcrypt/bcrypt.h
+++ include/libs/bcrypt/bcrypt.h
@@ -2,7 +2,7 @@
 #define BCRYPT_H
 
 #include "src/bcrypt_fwd.h"
-#include "src/bcrypt_.h"
+#include "src/bcrypt-essential.h"
 #include "src/openbsd.h"
 #include "src/node_blf.h"
 #include "src/blowfish.h"
