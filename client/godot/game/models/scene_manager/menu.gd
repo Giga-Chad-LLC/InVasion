baa44e092609310e1ea372f31ccd8bb42bfeae36
Dmitrii Art <artuhovdmitrii@gmail.com>
2022-06-03 21:10:32 +0300

    Remove old files

--- client/godot/game/models/scene_manager/menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-signal scene_changed(scene_name)
-
-onready var start_game_button = $CenterContainer/VBoxContainer/StartGameButton
-
-
-func _on_StartGameButton_pressed():
-	emit_signal("scene_changed", "game")
