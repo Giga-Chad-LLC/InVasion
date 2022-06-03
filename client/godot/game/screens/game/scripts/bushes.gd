4e70f4493675b6e4226c82a44fe1034985b02474
TommyIQ <bmp110676@gmail.com>
2022-06-03 21:31:05 +0300

    Update all file that store data to use global storing function

--- client/godot/game/screens/game/scripts/bushes.gd
+++ client/godot/game/screens/game/scripts/bushes.gd
@@ -1,12 +1,6 @@
 extends YSort
 
-var STORE_DATA_IN_FILE = false
-
-const DataSavingUtils = preload("res://utils/data_saving.gd")
-
 func _ready():
-	if(STORE_DATA_IN_FILE):
-		var utils = DataSavingUtils.new()
-		
-		var filename = utils.create_filename(self.name)
-		utils.writeChildrenWithRectangularShapeInFile(filename, self)
+	pass
+	#if (Global.STORE_DATA_IN_FILE):
+	#	Global.saveNodesChildrenCollisionShapesInFile(self)
--- client/godot/game/screens/game/scripts/monuments.gd
+++ client/godot/game/screens/game/scripts/monuments.gd
@@ -1,15 +1,5 @@
 extends YSort
 
-#var STORE_DATA_IN_FILE = false
-
-const DataSavingUtils = preload("res://utils/data_saving.gd")
-
 func _ready():
 	if (Global.STORE_DATA_IN_FILE):
 		Global.saveNodesChildrenCollisionShapesInFile(self)
-	
-	#if(STORE_DATA_IN_FILE):
-	#	var utils = DataSavingUtils.new()
-	#	
-	#	var filename = utils.create_filename(self.name)
-	#	utils.writeChildrenWithRectangularShapeInFile(filename, self)
