fb992c27d5e49e3dbea5ad7052f3800ce47c0952
TommyIQ <bmp110676@gmail.com>
2022-06-03 21:20:45 +0300

    Update monuments.gd to use generic saving function

--- client/godot/game/screens/game/scripts/monuments.gd
+++ client/godot/game/screens/game/scripts/monuments.gd
@@ -1,12 +1,15 @@
 extends YSort
 
-var STORE_DATA_IN_FILE = false
+#var STORE_DATA_IN_FILE = false
 
 const DataSavingUtils = preload("res://utils/data_saving.gd")
 
 func _ready():
-	if(STORE_DATA_IN_FILE):
-		var utils = DataSavingUtils.new()
-		
-		var filename = utils.create_filename(self.name)
-		utils.writeChildrenWithRectangularShapeInFile(filename, self)
+	if (Global.STORE_DATA_IN_FILE):
+		Global.saveNodesChildrenCollisionShapesInFile(self)
+	
+	#if(STORE_DATA_IN_FILE):
+	#	var utils = DataSavingUtils.new()
+	#	
+	#	var filename = utils.create_filename(self.name)
+	#	utils.writeChildrenWithRectangularShapeInFile(filename, self)
